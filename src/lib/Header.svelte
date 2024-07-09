e43983c26dbdaa1d864a44efe0d121d1f83ccd01
John Larson <jl@buildmidwestern.com>
2024-07-09 05:07:22 -0500

    feat: updated header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,23 +1,24 @@
 <script>
 	import { page } from '$app/stores';
 	import { Phone } from 'lucide-svelte';
+	import logo from '$lib/images/prince-logo.png';
 </script>
 
-<header class="flex flex-col items-center justify-center px-4 py-6 text-white bg-cyan-700">
-	<div class="flex flex-row items-center justify-between w-full">
+<header class="flex flex-col items-center justify-center px-4 py-6 text-blue-950 bg-neutral-200">
+	<div class="flex flex-row items-center justify-between w-full gap-3">
+		<a href="/" class="flex flex-row items-center justify-center w-64">
+			<img src={logo} alt="Prince CPA Firm, PLLC">
+		</a>
 		<div class="flex flex-row items-center justify-center w-64">
-			<h1 class="text-2xl font-semibold">Prince CPA Firm, PLLC</h1>
-		</div>
-		<div class="flex flex-row items-center justify-center w-64">
-			<a href="phone:+16018492544" class="flex flex-row items-center justify-center gap-3 text-lg font-medium">
-				<Phone stroke="white" size="28"/>
+			<a href="phone:+16018492544" class="flex flex-row items-center justify-center gap-3 text-base font-medium md:text-lg">
+				<Phone stroke="white" size="26"/>
 				<span>(601) 849-2544</span>
 			</a>
 		</div>
 	</div>
 
 	<nav>
-		<ul class="flex flex-row items-center gap-3 text-lg justify-stretch">
+		<ul class="flex flex-row items-center gap-3 text-lg font-medium justify-stretch">
 			<li class=" hover:pointer hover:underline" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
