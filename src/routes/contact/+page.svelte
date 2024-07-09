e88949deb1d7520635e7fe7a4732638e402066fc
John Larson <jl@buildmidwestern.com>
2024-07-09 13:43:22 -0500

    added layout for contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,5 +4,16 @@
 </svelte:head>
 
 <div>
-	<h1 class="text-sm bg-red-500 font-bold">contact</h1>
+	<h1 class="text-sm font-bold bg-red-500">contact</h1>
+	<!-- Need the following fields:
+	1. Name 
+	2. Email 
+	3. Phone Number 
+	4. Message
+	5. Dropdown to select if they are interested in an appointment
+	6. If they select yes, a calendar selection for their desired date and time 
+	7. Send button -->
+
+	<!-- Also add:
+	- Contact info box with same info as about page (bottom of page) -->
 </div>
