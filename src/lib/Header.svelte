1342365ecce8cee65f2aeb6c1b3b3116f7e8cfa0
John Larson <jl@buildmidwestern.com>
2024-07-09 05:26:57 -0500

    feat: updated header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,14 +4,20 @@
 	import logo from '$lib/images/prince-logo.png';
 </script>
 
-<header class="flex flex-col items-center justify-center px-4 py-6 text-blue-950 bg-neutral-200">
-	<div class="flex flex-row items-center justify-between w-full gap-3">
-		<a href="/" class="flex flex-row items-center justify-center w-64">
+<div class="flex flex-row items-center justify-center w-screen py-4 text-white sm:hidden bg-blue-950">
+	<a href="phone:+16018492544" class="flex flex-row items-center justify-center gap-3 text-base font-medium md:text-lg">
+		<Phone size={26}/>
+		<span>(601) 849-2544</span>
+	</a>
+</div>
+<header class="flex flex-col items-center justify-center gap-6 px-4 py-6 text-blue-950 bg-neutral-200">
+	<div class="flex flex-row items-center justify-center w-full gap-6 sm:justify-between">
+		<a href="/" class="flex flex-row items-center justify-center w-80">
 			<img src={logo} alt="Prince CPA Firm, PLLC">
 		</a>
-		<div class="flex flex-row items-center justify-center w-64">
-			<a href="phone:+16018492544" class="flex flex-row items-center justify-center gap-3 text-base font-medium md:text-lg">
-				<Phone stroke="white" size="26"/>
+		<div class="flex-row items-center justify-center hidden w-64 sm:flex">
+			<a href="phone:+16018492544" class="flex flex-row items-center justify-center gap-3 text-lg font-medium">
+				<Phone size={26}/>
 				<span>(601) 849-2544</span>
 			</a>
 		</div>
