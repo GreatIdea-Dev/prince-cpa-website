dfd6999b24f53fdcb113579df24ec6b104a71351
John Larson <jl@buildmidwestern.com>
2024-07-09 14:50:08 -0500

    updated header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -16,7 +16,7 @@
 	</a>
 </div>
 <header
-	class="flex flex-col items-center justify-center gap-6 px-4 py-6 text-blue-950 bg-neutral-200"
+	class="flex flex-col items-center justify-center gap-6 px-4 pt-6 text-blue-950 bg-neutral-200"
 >
 	<div class="flex flex-row items-center justify-center w-full gap-6 sm:justify-between">
 		<a href="/" class="flex flex-row items-center justify-center w-80">
@@ -33,31 +33,31 @@
 		</div>
 	</div>
 
-	<nav>
-		<ul class="flex flex-row items-center gap-3 text-lg font-medium justify-stretch">
-			<li
-				class=" hover:pointer hover:underline"
-				aria-current={$page.url.pathname === '/' ? 'page' : undefined}
-			>
-				<a href="/">Home</a>
+	<nav class="flex flex-row items-center justify-center w-screen bg-blue-950">
+		<ul class="flex flex-row font-medium text-white items-centertext-lg justify-stretch">
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a
+					class="flex items-center justify-center h-12 px-8 border-l-2 border-r border-white hover:cursor-pointer hover:bg-blue-900"
+					href="/">Home</a
+				>
 			</li>
-			<li
-				class=" hover:pointer hover:underline"
-				aria-current={$page.url.pathname === '/about' ? 'page' : undefined}
-			>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a
+					class="flex items-center justify-center h-12 px-8 border-white border-x hover:cursor-pointer hover:bg-blue-900"
+					href="/about">About</a
+				>
 			</li>
-			<li
-				class=" hover:pointer hover:underline"
-				aria-current={$page.url.pathname === '/services' ? 'page' : undefined}
-			>
-				<a href="/services">Services</a>
+			<li aria-current={$page.url.pathname === '/services' ? 'page' : undefined}>
+				<a
+					class="flex items-center justify-center h-12 px-8 border-white border-x hover:cursor-pointer hover:bg-blue-900"
+					href="/services">Services</a
+				>
 			</li>
-			<li
-				class=" hover:pointer hover:underline"
-				aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}
-			>
-				<a href="/contact">Contact</a>
+			<li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
+				<a
+					class="flex items-center justify-center h-12 px-8 border-l border-r-2 hover:cursor-pointer hover:bg-blue-900"
+					href="/contact">Contact</a
+				>
 			</li>
 		</ul>
 	</nav>
