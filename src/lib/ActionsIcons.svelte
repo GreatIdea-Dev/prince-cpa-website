847d4db6d4910a72929c086e47ce34edd00c3d44
Jedikay <kadyns0604@gmail.com>
2024-07-10 18:05:17 -0500

    chore:relocate pymt and login links

--- src/lib/ActionsIcons.svelte
+++ src/lib/ActionsIcons.svelte
@@ -8,6 +8,47 @@
 >
 	<div class="flex flex-col items-center justify-center w-full gap-8 lg:flex-row">
 		<a
+			href="https://swipesimple.com/links/lnk_d783542f"
+			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
+		>
+			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
+				<CreditCard stroke="white" size={56} />
+			</div>
+			<div class="flex flex-col">
+				<span class="pb-1 text-2xl font-semibold">Make a Payment</span>
+				<p>Quickly make credit/debit card payments towards your account.</p>
+				<CtaLink text="Make a Payment" />
+			</div>
+		</a>
+		<a
+			href="https://secure.netlinksolution.com/nextgen/#/"
+			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
+		>
+			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
+				<Files stroke="white" size={56} />
+			</div>
+			<div class="flex flex-col">
+				<span class="pb-1 text-2xl font-semibold">Client Portal</span>
+				<p>Click here to login to your secure client portal.</p>
+				<CtaLink text="Client Login" />
+			</div>
+		</a>
+	</div>
+	<div class="flex flex-col items-center justify-center w-full gap-8 lg:flex-row">
+		<a
+			href="/contact"
+			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
+		>
+			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
+				<Calendar stroke="white" size={56} />
+			</div>
+			<div class="flex flex-col">
+				<span class="pb-1 text-2xl font-semibold">We Are Here to Help</span>
+				<p>We will happily offer you a free consultation to determine how we can best serve you.</p>
+				<CtaLink text="Contact Us" />
+			</div>
+		</a>
+		<a
 			href="/contact"
 			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
 		>
@@ -22,47 +63,6 @@
 					>Find comfort in knowing an expert in accounting is only an email or phone-call away.</span
 				>
 				<CtaLink text="(601) 849-2544" />
-			</div>
-		</a>
-		<a
-			href="/contact"
-			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
-		>
-			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
-				<Calendar stroke="white" size={56} />
-			</div>
-			<div class="flex flex-col">
-				<span class="pb-1 text-2xl font-semibold">We Are Here to Help</span>
-				<p>We will happily offer you a free consultation to determine how we can best serve you.</p>
-				<CtaLink text="Contact Us" />
-			</div>
-		</a>
-	</div>
-	<div class="flex flex-col items-center justify-center w-full gap-8 lg:flex-row">
-		<a
-			href="https://secure.netlinksolution.com/nextgen/#/"
-			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
-		>
-			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
-				<Files stroke="white" size={56} />
-			</div>
-			<div class="flex flex-col">
-				<span class="pb-1 text-2xl font-semibold">Client Portal</span>
-				<p>Click here to login to your secure client portal.</p>
-				<CtaLink text="Client Login" />
-			</div>
-		</a>
-		<a
-			href="https://swipesimple.com/links/lnk_d783542f"
-			class="flex flex-row items-center justify-start w-full gap-3 group lg:w-1/2 hover:cursor-pointer"
-		>
-			<div class="p-4 text-white rounded-full bg-cyan-600 group-hover:bg-orange-600">
-				<CreditCard stroke="white" size={56} />
-			</div>
-			<div class="flex flex-col">
-				<span class="pb-1 text-2xl font-semibold">Make a Payment</span>
-				<p>Quickly make credit/debit card payments towards your account.</p>
-				<CtaLink text="Make a Payment" />
 			</div>
 		</a>
 	</div>
