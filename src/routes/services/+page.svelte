c22c8092950550a353be568907a3ca02daf1b9d9
Jedikay <kadyns0604@gmail.com>
2024-07-09 22:55:48 -0500

    init:setup

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -14,25 +14,317 @@
 			regulatory environment.
 		</p>
 		<p>Our professional services include:</p>
-		<ul style="list-style-type:circle">
+		<ul style="list-style-type:disc">
 			<li>Debt and Finance Advising</li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			<li>Accounting Services</li>
+			<li>Audits, Reviews, and Compilation</li>
+			<li>Financial and Retirement Planning</li>
+			<li>Bookkeeping/Write-up</li>
+			<li>IRS Representation</li>
+			<li>Payroll Services</li>
+			<li>Cash Flow and Budgeting Anaysis</li>
+			<li>Debt and Finance Advising</li>
+		</ul>
+		<ul style="list-style-type:disc">
+			<li>Tax Managment Services</li>
+			<li>Assurance and Advisory Services</li>
+			<li>Estate and Trust Planning and Tax Preparation</li>
+			<li>OuickBooks Accounting Help and Assistance</li>
+			<li>Entity Selection and Restructuring</li>
+			<li>Computer Consulting</li>
+			<li>Employee Benefits, Pension, and Profit-Sharing Plans</li>
+			<li>Financial Projections and Forecasts</li>
+			<li>Foreign Taxation</li>
 		</ul>
 	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Debt and Finance Advising</h2>
+		<p>
+			Whether your needs are corporate or personal, Prince CPA Firm, PLLC's professionals can assist
+			you in sorting out the different options available for debt management and financing that will
+			lower your payments and/or reduce the total amount of interest paid. Re-balancing your debts
+			can also result in tax deduction savings. Even if you are able to handle the payments and the
+			amounts of your current debts, our expertise may be able to save you money and/or lower your
+			payments.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Tax Management Services</h2>
+		<p>
+			At Prince CPA Firm, PLLC, we guide our clients through a full range of tax planning and
+			preparation decisions with strategies that minimize your tax liabilities, maximize your cash
+			flow and keep you on track to your financial goals. Our expertise, experience, analysis and
+			thorough research allow us to optimize financial opportunities to be found in existing as well
+			as recently altered tax laws. We are knowledgeable and up to date on the tax laws and can make
+			sense of your receipts, bills and notices.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Accounting Services</h2>
+		<p>
+			From start-ups to established enterprises, businesses rely on accurate and insightful
+			financial information in order to maintain profitability and capitalize on new opportunities.
+			Prince CPA Firm, PLLC's accounting services steer you closer to these goals with accurate
+			record-keeping and reporting as well as support on financial issues such as initial accounting
+			system setup, cost-containment, tax planning, investments, and employee benefit and
+			profit-sharing plans.
+		</p>
+		<p>These services include but are not limited to</p>
+		<ul style="list-style-type:disc">
+			<li>General ledger and financial statement preparation</li>
+			<li>Bookkeeping(Monthly, quarterly, or annual)</li>
+			<li>Accounting system setup and support</li>
+			<li>Payroll processing</li>
+			<li>Cash flow budgeting and forecasting</li>
+			<li>Personal financial statements</li>
+			<li>Employee benefit and profit-sharing plans</li>
+			<li>Corporate tax planning and return preparation</li>
+			<li>Litigation support</li>
+		</ul>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Assurance and Advisory Services</h2>
+		<p>
+			From start-ups to established enterprises, businesses rely on accurate and insightful
+			financial information in order to maintain profitability and capitalize on new opportunities.
+			Prince CPA Firm, PLLC's accounting services steer you closer to these goals with accurate
+			record-keeping and reporting as well as support on financial issues such as initial accounting
+			system setup, cost-containment, tax planning, investments, and employee benefit and
+			profit-sharing plans.
+		</p>
+		<p>These services include but are not limited to</p>
+		<ul style="list-style-type:disc">
+			<li>Audits, reviews, and compilations</li>
+			<li>Entity selection and restructuring</li>
+			<li>Accounting software selection, implementation, and support</li>
+			<li>Business valuations</li>
+			<li>Financial projections and forecasts</li>
+			<li>Debt and finances advising</li>
+			<li>Mergers and acquistions</li>
+			<li>Transaction structuring and negotiations</li>
+			<li>Business succession</li>
+		</ul>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Audits, Reviews, and Compilation</h2>
+		<p>
+			Prince CPA Firm, PLLC's financial statement services comprise all three levels of assurance
+			depending on your company's needs.
+		</p>
+		<p class="text-sm font-bold">Audits</p>
+		<p>
+			Audits offer the highest level of assurance to third parties, and include in-depth examination
+			and confirmation of account balances, inventories, and selected transactions. We perform
+			audits for you to remove errors, ascertaining the validity and reliability of your
+			information.
+		</p>
+		<p class="text-sm font-bold">Reviews</p>
+		<p>
+			Reviews provide limited assurance to outside interests and involve inquiries and analytical
+			procedures that confirm financial statement matters and identify any items requiring further
+			analysis. We perform reviews to ensure the integrity of your data.
+		</p>
+		<p class="text-sm font-bold">Compilations</p>
+		<p>
+			Compilations are usually requested for internal purposes and are based upon information
+			provided by a company's management. They do not offer assurance but may involve some
+			adjustment to accounting records.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">
+			Estate and Trust Planning and Tax Prepartaion
+		</h2>
+		<p>
+			Effective estate and trust planning can ensure financial security for loved ones. For
+			businesses, it can maintain a smooth succession of ownership. Prince CPA Firm, PLLC's role is
+			to help you navigate the complex and shifting tax laws to facilitate the transfer of assets
+			and minimize the tax liability of your beneficiaries. Everyone should have a well-thought-out
+			plan as to how to distribute the assets left in one's estate so as to avoid complications and
+			to be sure that the individual’s wishes are followed. Having your taxes and estate thoroughly
+			and carefully planned will ensure that your loved ones don't have any complications to deal
+			with during a time of loss.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Financial and Retirement Planning</h2>
+		<p>
+			Prince CPA Firm, PLLC's goal is to help you reach your financial goals and maintain financial
+			independence through a comfortable retirement. We can guide you through tough decision-making
+			processes such as asset allocation, investment performance monitoring, college education
+			funding, retirement plans, insurance, and tax, estate, and business succession planning.
+			Whether you are developing an investment strategy or evaluating current investments, Prince
+			CPA Firm, PLLC can ensure that your portfolio will take into account tax implications for an
+			advantageous after-tax return. The road to retirement is a long one and we can help you to
+			take the proper steps to reach your goals.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Quickbooks Accounting Help and Assistance</h2>
+		<p>
+			QuickBooks can provide useful and timely information in the form of financial statements,
+			reports and graphs. However, it can only provide this information if you purchase the right
+			product and then install, setup and use it properly. We don't just help you use the software,
+			we help you use it more efficiently and more effectively.
+		</p>
+		<p>Prince CPA Firm, PLLC offers the following solutions to help meet your QuickBooks needs:</p>
+		<p class="text-sm font-bold">Personal Training</p>
+		<p>
+			We train you personally! Either one-on-one or several individuals within your organization at
+			your company's location.
+		</p>
+		<p class="text-sm font-bold">Installation</p>
+		<p>
+			We install QuickBooks on your stand alone computer or in a network environment, in either
+			single- or multi-user mode
+		</p>
+		<p class="text-sm font-bold">Setup</p>
+		<p>
+			We assist new QuickBooks users with initial setup including: EasyStep Interview, Preferences,
+			Lists, Customers, Vendors, Employees, Banking and Reports. We also help experienced users
+			manage their businesses more effectively by improving their current setup. Many users
+			experience problems and lack the ability to generate and track important information as a
+			result of inadequate setup.
+		</p>
+		<p class="text-sm font-bold">Support</p>
+		<p>
+			Our QuickBooks support service can assist you with any installation, setup or operation
+			assistance you might need. In addition, our support is not limited just to the software part
+			of it, but we can also provide accounting and payroll assistance.
+		</p>
+		<p class="text-sm font-bold">Review</p>
+		<p>
+			Our QuickBooks review service helps companies that have the human resources to perform daily
+			accounting and payroll tasks, but need an accounting and payroll expert to review your
+			transactions, accounts and reports. This review ensures that you receive timely, relevant and
+			reliable financial information. We also inform you of any corrections, adjustments or
+			reclassifications necessary to ensure that the financial information you receive reflects the
+			correct financial condition of your business. Reviews may be conducted at any time, but
+			monthly reviews provide you with up-to-date information and feedback about your business.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Debt and Finance Advising</h2>
+		<p>
+			Whether your needs are corporate or personal, Prince CPA Firm, PLLC's professionals can assist
+			you in sorting out the different options available for debt management and financing that will
+			lower your payments and/or reduce the total amount of interest paid. Re-balancing your debts
+			can also result in tax deduction savings. Even if you are able to handle the payments and the
+			amounts of your current debts, our expertise may be able to save you money and/or lower your
+			payments.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Bookkeeping/Write-Up</h2>
+		<p>
+			Whether your needs are corporate or personal, Prince CPA Firm, PLLC's professionals can assist
+			you in sorting out the different options available for debt management and financing that will
+			lower your payments and/or reduce the total amount of interest paid. Re-balancing your debts
+			can also result in tax deduction savings. Even if you are able to handle the payments and the
+			amounts of your current debts, our expertise may be able to save you money and/or lower your
+			payments.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Entity Selection and Restructuring</h2>
+		<p>
+			Your business entity has a large impact on your taxes and other liabilities. From your
+			company's inception through its growth and development, Prince CPA Firm, PLLC can advise you
+			on choosing an entity type and later restructuring if advantageous. With our knowledge and
+			expertise, you will always be receiving the most advantageous entity type for the functions
+			your business performs.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">IRS Representation</h2>
+		<p>
+			Professional representation can be vital during an audit, and our experience with tax
+			authorities enables us to guide clients in their dealings with federal and state agencies. If
+			you have been chosen for an audit, the professional representation you can find with our firm
+			can put many of your worries at bay. We are ready and willing to answer any and all questions
+			the IRS may be asking of you.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Computer Consulting</h2>
+		<p>
+			As technologies rapidly advance, companies increasingly need to update their computer systems.
+			Prince CPA Firm, PLLC can audit your system competencies and gauge upgrade needs as well as
+			provide support in choosing and implementing new hardware or network systems. As experts in
+			this field, we can put your computer problems at bay as well as improve your computer system's
+			performance taking into consideration any budget you may have.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Payroll Services</h2>
+		<p>
+			Prince CPA Firm, PLLC's payroll services can help you reduce the time spent on administration
+			through developing and implementing a computerized payroll system that will facilitate
+			processing, timely payment and preparation of tax returns.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">
+			Employee Benefits, Pension, and Profit-Sharing Plans
+		</h2>
+		<p>
+			Choosing and administering benefit plans for your employees is often a complex process. Prince
+			CPA Firm, PLLC can help you develop, implement, and administer your benefit, retirement, and
+			profit-sharing plans with an eye toward rewarding your employees and reaching your company's
+			financial and business goals. We can also help maximize tax advantages in implementing these
+			plans. As your business grows, your business and your employees will be rewarded for the hard
+			work that contributed to the success of your business.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Cash Flow and Budgeting Analysis</h2>
+		<p>
+			Good cash management can improve a company's liquidity, reduce costs, and increase
+			profitability. Prince CPA Firm, PLLC can help you maintain optimal cash flow levels by
+			tracking sources and uses, forecasting, and budgeting accordingly. To a business entity, cash
+			flow is something that can make or break the business' ability to survive. We can help you
+			analyze your spending, re-balance your budget and/or debts for an optimal cash flow to support
+			your business' success. This balance plan would be revisited if and when there were any major
+			changes in your business structure to ensure that you are operating at an optimal level. With
+			our help and guidance, you will always be on top of your finances and ready for the future.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Finacial Projections and Forecasts</h2>
+		<p>
+			No one can predict the future perfectly, but we can all benefit from planning for it.Prince
+			CPA Firm, PLLC combines expertise and experience with a gained understanding of your business
+			to produce financial projections that can help you manage your business plan and spending.
+			Forecasting for the future is imperative to your company's success. We make it a point to keep
+			current with market trends and updates so that your business can be prepared for what is to
+			come. Depending on your needs, our work can range from top-level reports to detailed financial
+			models.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<h2 class="text-xl font-semibold text-blue-950">Foreign Taxation</h2>
+		<p>
+			Our experience with residents working abroad has provided us with an extensive base of
+			knowledge in the area of foreign taxation.
+		</p>
+	</div>
+	<hr class="w-full border border-blue-950" />
 </div>
