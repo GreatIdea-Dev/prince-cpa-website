06d7dcdea97dc2ada6502a6efa2c64978d3ec6fd
Jedikay <kadyns0604@gmail.com>
2024-07-09 20:20:55 -0500

    init:setup

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -3,6 +3,36 @@
 	<meta name="description" content="Prince CPA Firm, PLLC" />
 </svelte:head>
 
-<div>
-	<h1 class="text-sm bg-red-500 font-bold">Services</h1>
+<div class="flex flex-col items-start justify-center gap-5 p-8">
+	<h1 class="w-full text-2xl font-bold text-center text-blue-950">Services</h1>
+	<hr class="w-full border border-blue-950" />
+	<div class="flex flex-col gap-4">
+		<p>
+			Prince CPA Firm, PLLC provides a wide range of services to individuals and businesses in a
+			variety of industries. At Prince CPA Firm, PLLC, we strive to meet each client's specific
+			needs in planning for the future and achieving their goals in an ever-changing financial and
+			regulatory environment.
+		</p>
+		<p>Our professional services include:</p>
+		<ul style="list-style-type:circle">
+			<li>Debt and Finance Advising</li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
 </div>
